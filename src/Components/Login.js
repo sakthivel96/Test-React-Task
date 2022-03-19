19adecfc8272e2a508e342f2fabd366f7e4ffb89
Sakthivel <velsakthi996@gmail.com>
2022-03-19 16:44:51 +0530

    Update1

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -116,7 +116,7 @@ const Login = () => {
 
   return (
     <div className="login-form">
-      <p>Login Form test working</p>
+      <p>Login Form test working!!!</p>
       <form onSubmit={loginSubmit}>
         <input type="text" placeholder="Email" onChange={setUsernameMet} 
         />
